3be31106d2fe59a14d04a94353444d35ab61f529
Radu Cotescu <radu@cotescu.com>
2011-05-10 14:49:04 +0100

    temporary edit

--- include/expressions.hpp
+++ include/expressions.hpp
@@ -13,9 +13,59 @@
  * that provides code for evaluating the expression. This function should accept
  * a double parameter that will be used as the value for the single variable x.
  */
-
 #ifndef EXPRESSIONS_HPP_
 #define EXPRESSIONS_HPP_
+using namespace std;
 
+/*
+ * Class defining Literals.
+ */
+class Literal {
+public:
+	Literal(const double v) : value(v) {}
+	double eval() const {
+		return value;
+	}
+private:
+	const double value;
+};
 
+/*
+ * Class defining variables.
+ */
+class Variable {
+	Variable(const double v) : value(v) {}
+	double eval() const {
+		return value;
+	}
+	;
+private:
+	double& value;
+};
+
+/*
+ * Template defining a binary expression.
+ */
+template <class LHS, class RHS, class BinaryOperation> class BinaryExpression {
+public:
+	BinaryExpression(
+			LHS _lhs,
+			RHS _rhs,
+			BinaryOperation _operation = BinaryOperation()
+	) : lhs(_lhs), rhs(_rhs), operation(_operation) {}
+	double eval() const {
+		return operation(lhs.eval(), rhs.eval());
+	}
+private:
+	LHS lhs;
+	RHS rhs;
+	BinaryOperation operation;
+};
+
+/*
+ * Operator overriding.
+ */
+template <class LHS, class RHS> BinaryExpression<LHS, RHS, plus<double> > operator+(LHS lhs, RHS, rhs) {
+	return BinaryExpression<LHS, RHS, plus<double> >(lhs, rhs);
+}
 #endif /* EXPRESSIONS_HPP_ */
