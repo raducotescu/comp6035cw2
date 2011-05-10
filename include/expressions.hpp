3f3639ba7c2b16846a4008412fb0a47c903bde5f
Radu Cotescu <radu@cotescu.com>
2011-05-10 01:19:16 +0100

    added template file for expressions

---
+++ include/expressions.hpp
@@ -0,0 +1,21 @@
+/*
+ * expressions.hpp
+ * Author: Radu Cotescu (rdc1g10)
+ *
+ * Part 2: Expression Templates
+ * Write C++ templates which allow you to use types to represent arithmetic
+ * expressions over the four basic operations (addition, subtraction,
+ * multiplication and division), a single variable, and integer exponentiation:
+ * (e.g. x^2+(x-2)^3/5). For the purposes of this assignment we refer to the
+ * single variable as x.
+ *
+ * The types representing the expressions should contain a function named "eval"
+ * that provides code for evaluating the expression. This function should accept
+ * a double parameter that will be used as the value for the single variable x.
+ */
+
+#ifndef EXPRESSIONS_HPP_
+#define EXPRESSIONS_HPP_
+
+
+#endif /* EXPRESSIONS_HPP_ */
